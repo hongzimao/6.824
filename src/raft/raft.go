21b4b5042e8986e6172434d93893e9f7e635166c
Hongzi Mao <Hongzi@Hongzis-MacBook-Pro.local>
2016-04-24 22:22:17 -0400

    Lab 2 3 4a GM

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -26,6 +26,7 @@ import (
 	"encoding/gob"
 	// "fmt"
 	)
+
 //
 // as each Raft peer becomes aware that successive log entries are
 // committed, the peer should send an ApplyMsg to the service (or
